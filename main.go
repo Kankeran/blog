aaab7773ee539fb01382aea5db14c9f0df6fdf41
AwIlL <awillcezo@gmail.com>
2022-10-28 22:01:57 +0200

    initial web service

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func Wiktror(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	parsedTemplate, _ := template.ParseFiles("./home.html")
+	err := parsedTemplate.Execute(w, ps.ByName("name"))
+	check(err)
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/wiktror/:name", Wiktror)
+	router.ServeFiles("/static/*filepath", http.Dir("./static"))
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
